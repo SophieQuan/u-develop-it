e9412b5b31a3695e54cd1d3e2fe92a79726dc5c5
SophieQuan <sophiequan7@gmail.com>
2022-03-07 21:13:03 -0500

    Create MySQL Queries

--- server.js
+++ server.js
@@ -24,9 +24,30 @@ const db = mysql.createConnection({
 app.use((req, res) => {
     res.status(404).end();
 });
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 //function that will start the Express.js server on port 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
