7af0651bd9b09b6f4c510489ca0d636416bd1101
SophieQuan <sophiequan7@gmail.com>
2022-03-07 21:04:12 -0500

    Build the Database Calls

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//import and configure dotenv:
+require('dotenv').config()
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.password,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 //function that will start the Express.js server on port 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
